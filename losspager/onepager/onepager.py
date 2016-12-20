199719d274634534665982798cdef75809c20199
Thompson <emthompson@usgs.gov>
2016-12-20 14:01:30 -0700

    Rewrote onepager layout using tikz library

--- losspager/onepager/onepager.py
+++ losspager/onepager/onepager.py
@@ -38,7 +38,7 @@ def create_onepager(version_dir, debug = False):
     data_dir = os.path.join(losspager_dir, 'data')
 
     # Onepager latex template file
-    template_file = os.path.join(data_dir, 'onepager.tex')
+    template_file = os.path.join(data_dir, 'onepager2.tex')
 
     #---------------------------------------------------------------------------
     # Read in pager data and latex template
@@ -164,14 +164,14 @@ def create_onepager(version_dir, debug = False):
     else:
         # build latex table
         htex = """
-\\begin{tabularx}{6.6cm}{lrc*{1}{>{\\centering\\arraybackslash}X}r}
+\\begin{tabularx}{7.25cm}{lrc*{1}{>{\\centering\\arraybackslash}X}r}
 \hline
 \\textbf{Date} &\\textbf{Dist.}&\\textbf{Mag.}&\\textbf{Max}    &\\textbf{Shaking}\\\\
 \\textbf{(UTC)}&\\textbf{(km)} &              &\\textbf{MMI(\#)}&\\textbf{Deaths} \\\\
 \hline
 [TABLEDATA]
 \hline
-\multicolumn{5}{p{6.5cm}}{\\raggedright \\footnotesize [COMMENT]}
+\multicolumn{5}{p{7.2cm}}{\\raggedright \\footnotesize [COMMENT]}
 \end{tabularx}"""
         comment = pdata._pagerdict['comments']['secondary_comment']
         htex = htex.replace("[COMMENT]", comment)
@@ -199,14 +199,12 @@ def create_onepager(version_dir, debug = False):
 
     # City table
     ctex = """
-\\begin{tabularx}{6.6cm}{lXr}
+\\begin{tabularx}{7.25cm}{lXr}
 \hline
 \\textbf{MMI} & \\textbf{City} & \\textbf{Population}  \\\\
 \hline
 [TABLEDATA]
 \hline
-\multicolumn{2}{l}{\\footnotesize bold cities appear on map} &
-\\footnotesize (k = x1000)\\\\
 \end{tabularx}"""
     ctab = pdata.getCityTable()
     nrows = len(ctab.index)
