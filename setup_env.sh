989423dd4b98e70d3cd3c71f6f7106661112eecc
Michael Hearne <mhearne@usgs.gov>
2017-10-25 08:58:38 -0600

    Fixed a dependency on hypothesis, a pytest plugin.

--- setup_env.sh
+++ setup_env.sh
@@ -6,13 +6,13 @@ PYVER=3.6
 DEPARRAY=(numpy=1.12.1 scipy=0.19.0 matplotlib=1.5.3 jupyter=1.0.0 \
           rasterio=0.36.0 fiona=1.7.6 xlrd=1.0.0 xlwt=1.2.0 \
           pandas=0.20.2 shapely=1.5.17 h5py=2.7.0 gdal=2.1.3 \
-          pyproj=1.9.5.1 pytest=3.1.2 pytest-cov=2.5.1 \
+          pyproj=1.9.5.1 pytest=3.2.3 pytest-cov=2.5.1 \
           pytables=3.4.2 pytest-mpl=0.7 cartopy=0.15.1 \
           pyyaml=3.12 rtree=0.8.3 openpyxl=2.5.0a1 \
           pycrypto=2.6.1 paramiko=2.1.2 beautifulsoup4=4.5.3 \
           docutils=0.13.1 decorator=4.0.11 nose=1.3.7 mock=2.0.0 \
           cython=0.25.2 sqlalchemy=1.1.11 sqlalchemy-utils=0.32.12 \
-          descartes=1.1.0 flake8=3.4.1)
+          descartes=1.1.0 flake8=3.4.1 hypothesis=3.21.1)
 
 #if we're already in an environment called pager, switch out of it so we can remove it
 source activate root
@@ -55,7 +55,7 @@ pip install mapio.zip
 rm mapio.zip
 
 #download impactutils, install it using pip locally
-curl --max-time 60 --retry 3 -L https://github.com/usgs/earthquake-impact-utils/archive/master.zip -o impact.zip
+curl --max-time 60 --retry 3 -L https://github.com/usgs/earthquake-impact-utils/archive/0.5.zip -o impact.zip
 pip install impact.zip
 rm impact.zip
 
