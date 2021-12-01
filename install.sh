34184059ff90371ea28fcf0519e53ed9234bbe4e
Mike Hearne <mhearne@usgs.gov>
2021-12-01 12:27:51 -0700

    Making some changes to install setup, hoping to simplify installation on linux systems at least

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ source $prof
 VENV=pager
 
 # Is the reset flag set?
-reset=1
+reset=0
 while getopts r FLAG; do
   case $FLAG in
     r)
@@ -87,13 +87,7 @@ if [ $? -ne 0 ]; then
     echo ". $HOME/miniconda/etc/profile.d/conda.sh" >> $prof
 fi
 
-# If the user has specified the -r (reset) flag, then create an
-# environment based on only the named dependencies, without
-# any versions of packages specified.
-if [ $reset == 1 ]; then
-    echo "Ignoring platform, letting conda sort out dependencies..."
-    env_file=environment.yml
-fi
+
 
 # Start in conda base environment
 echo "Activate base virtual environment"
@@ -164,21 +158,32 @@ conda config --add channels 'conda-forge'
 conda config --add channels 'defaults'
 conda config --set channel_priority strict
 
-# Create a conda virtual environment
-echo "Creating the $VENV virtual environment:"
-conda create -n $VENV python=3.7 -y
+# If the user has specified the -r (reset) flag, then create an
+# environment based on only the named dependencies, without
+# any versions of packages specified.
+if [ $reset != 1 ]; then
+    echo "Installing PAGER from ${env_file}..."
+    conda env create -f $env_file
+else
+    echo "Ignoring platform, letting conda sort out dependencies..."
+    # Create a conda virtual environment
+    echo "Creating the $VENV virtual environment:"
+    conda create -n $VENV python=3.7 -y
 
-# activate the new environment so mamba knows where to install packages
-echo "Activating ${VENV} environment..."
-conda activate $VENV
+    # activate the new environment so mamba knows where to install packages
+    echo "Activating ${VENV} environment..."
+    conda activate $VENV
 
-# Use mamba to install packages
-echo "Using mamba to solve dependencies and install packages..."
-mamba install -y $package_list
+    # Use mamba to install packages
+    echo "Using mamba to solve dependencies and install packages..."
+    mamba install -y $package_list
+fi
+
+
 
 # Bail out at this point if the conda create command fails.
 # Clean up zip files we've downloaded
-if [ $? -ne 0 ]; then
+if [ $? != 0 ]; then
     echo "Failed to create conda environment.  Resolve any conflicts, then try again."
     exit
 fi
