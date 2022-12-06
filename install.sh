2b8c24a5db95fc6d8013003c8b78ed824e190acb
Mike Hearne <mhearne@usgs.gov>
2022-12-06 13:12:00 -0700

    Changing conda channel priorities, cleaning up some new errors with mapping that popped up with updated dependencies

--- install.sh
+++ install.sh
@@ -102,10 +102,10 @@ conda remove -y --name $VENV --all
 
 # define the list of packages
 package_list='
-  cartopy=0.20.2
+  cartopy>=0.21
   fiona>=1.8.13
   flake8
-  gdal=3.4.2
+  gdal>=3.4.2
   h5py>=2.10.0
   impactutils
   jupyter
@@ -113,9 +113,9 @@ package_list='
   mapio
   matplotlib>=3.5
   mock
-  numpy=1.21
+  numpy>=1.21
   openpyxl>=3.0
-  openssl=1.1.1
+  openssl>=1.1.1
   pandas>=1.4.2
   paramiko>=2.8.1
   pip
@@ -135,11 +135,6 @@ package_list='
   xlrd>=2.0.1
   xlwt'
 
-# it seems now that some of the geospatial packages are more stable
-# in the defaults channel, so let's set that as our preferred channel.
-conda config --add channels 'conda-forge'
-conda config --add channels 'defaults'
-conda config --set channel_priority flexible
 
 echo "Creating conda virtual environment..."
 # Create a conda virtual environment
@@ -169,34 +164,6 @@ conda activate $VENV
 # This package
 echo "Installing ${VENV}..."
 pip install -e .
-
-# test pager, if we get an error about libffi, try to fix it
-# this is a total hack, but the only way I can see to get around 
-# this weird result.
-ffi_lib=libffi.so.7
-pager_lib_dir=~/miniconda/envs/pager/lib
-test_res=$(pager -h 2>&1)
-echo $test_res | grep "${ffi_lib}"
-if [ $? == 0 ]; then
-    echo "Issue finding library ${ffi_lib}. Trying to resolve..."
-    ffi_files=$(find ~/miniconda -name "libffi.so.7")
-    if [ -z "$ffi_files" ]; then
-        # this library is not found on the system
-        echo "Cannot find missing library ${ffi_lib}. Please attempt to sort out libffi library issue."
-        exit 1
-    fi
-    ffi_file=$(echo $ffi_files | head -1)
-    echo "Copying ${ffi_file} to ${pager_lib_dir}..."
-    cp $ffi_file $pager_lib_dir
-    pager -h 2>/dev/null
-    if [ $? != 0 ]; then
-        echo "pager is still broken. Please address this manually."
-        exit 1
-    fi
-fi
-
-# Install default profile
-#python bin/sm_profile -c default -a
 
 # Tell the user they have to activate this environment
 echo "Type 'conda activate $VENV' to use this new virtual environment."
